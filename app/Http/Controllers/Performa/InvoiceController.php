969a1f95e3edfb850d27b32a0bbae607077005b7
Vraj Surani <vrajsurani606@gmail.com>
2025-12-18 19:51:22 +0530

    Delete app directory

--- app/Http/Controllers/Performa/InvoiceController.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Http\Controllers\Performa;
-use App\Http\Controllers\Controller;
-class InvoiceController extends Controller
-{
-    public function index(){ return view('invoices.index'); }
-    public function show($id){ return view('invoices.show', compact('id')); }
-}
--- app/Http/Controllers/Setting/SettingController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Http\Controllers\Setting;
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-class SettingController extends Controller
-{
-    public function index(){ return view('section',['name'=>'settings']); }
-    public function update(Request $r){ return back()->with('success','Settings updated'); }
-}
